bad09a73e6561d129c29098e385e6122ee6ad5e0
cruzelx <cruze.alex@gmail.com>
2018-06-11 22:55:35 +0545

    static files

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const express = require('express');
-const app = express();
+var express = require("express");
+var app = express();
+var path = require('path');
 
 app.get('/',(req,res)=>res.send("<h1>ROOT</h1>"));
 app.get('/alex',(req,res)=>res.send("<h1>Alex Page</h1>"));
-app.listen(3000,()=>console.log('listening...'))
+app.post('/',(req,res)=>res.send("<h1>Post Request</h1>"));
+app.use('/static',express.static(path.join(__dirname,'public')));
+app.all('/serve',(req,res,next)=>{
+    res.send("<h1>Server</h1>");
+    next();
+});
+app.set('port',process.env.PORT || 8000);
+app.listen(app.get('port'),()=>console.log('listening...'));
